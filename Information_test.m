ff0a570c52560b36ca33331e322d22464a5d090c
cannicj <jannicnoel.cavegn@uzh.ch>
2023-03-14 13:14:41 +0100

    first push, testing repository

---
+++ Information_test.m
@@ -0,0 +1,7 @@
+% This is a test to see if the set up of the new github repository has
+% worked. This github will serve as the main overview of all coding files,
+% separated by assignments in the sub-folders.
+
+% The whole documentation and creation of the report is being done with
+% Overleaf, where Latex can be used very easily. A template has already
+% been created and been made accessible.
