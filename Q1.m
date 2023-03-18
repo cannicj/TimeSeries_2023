c31107904f917d6900bd70f4bbcfe707dc4dfde0
cannicj <jannicnoel.cavegn@uzh.ch>
2023-03-18 18:47:44 +0100

    changed regression to using formulas in the book to calculate R^2 and F statistic instead of built in Matlab version (old way still in the codes, clean up still needed) changed the way of creating the graphs for the second true beta (1,1,1,1) by splitting it in one graph for the R^2 and one for the F-statistic. F-statistic needs to be worked on, still seems to be wrong

--- Q1.m
+++ Q1.m
@@ -24,6 +24,30 @@ for j = 1:length(sigmas)
     end
 end
 
+%% Simuation for test 1 by not using built in regression
+% Simulate for test one
+R2_test_one = zeros(sim, 3);
+F_test_one = zeros(sim, 3);
+for j = 1:length(sigmas)
+    sigma = sigmas(j);
+    for i = 1:sim
+        e = normrnd(0,sqrt(sigma),n,1); % Residual vector
+        y = X * beta0 + e; % Response vector
+        % OLS regression
+        betaHat = inv((X' * X))*X'*y;
+        yHat = X * betaHat;
+        RSS = sum((y - yHat).^2);
+        TSS = sum((y - mean(y)).^2);
+        R2 = 1 - RSS/TSS; % save here for clearer use later on
+        R2_test_one(i,j) = R2;
+        F_test_one(i,j) = ((n-p+1)/p)*(R2/(1-R2));
+        %[b, ~, r, ~, stats] = regress(y, X);
+        % R^2 and F statistics
+        %R2_test_one(i, j) = stats(1);
+        %F_test_one(i, j) = stats(2);
+    end
+end
+
 
 %%
 % Simulate for test two
@@ -43,9 +67,34 @@ for j = 1:length(sigmas)
 end
 
 
+%% Simuation for test 2 by not using built in regression
+% Simulate for test two
+R2_test_two = zeros(sim, 3);
+F_test_two = zeros(sim, 3);
+for j = 1:length(sigmas)
+    sigma = sigmas(j);
+    for i = 1:sim
+        e = normrnd(0,sqrt(sigma),n,1); % Residual vector
+        y = X * beta1 + e; % Response vector
+        % OLS regression
+        betaHat = inv((X' * X))*X'*y;
+        yHat = X * betaHat;
+        RSS = sum((y - yHat).^2);
+        TSS = sum((y - mean(y)).^2);
+        R2 = 1 - RSS/TSS; % save here for clearer use later on
+        R2_test_two(i,j) = R2;
+        F_test_two(i,j) = ((n-p+1)/p)*(R2/(1-R2));
+        %[b, ~, r, ~, stats] = regress(y, X);
+        % R^2 and F statistics
+        %R2_test_two(i, j) = stats(1);
+        %F_test_two(i, j) = stats(2);
+    end
+end
+
+
 %%
- % Plot kernel density estimate of simulated distribution
- for s=1:3
+% Plot kernel density estimate of simulated distribution
+for s=1:3
     figure
     hold on
     x = linspace(0,1,1000);
@@ -54,30 +103,46 @@ end
     plot(x,yR2,'LineWidth',2)
     plot(x,yF,'LineWidth',2)
     % Plot theoretical null distributions
-    plot(x,betapdf(x,p,n-p),'--','LineWidth',2)
-    plot(x,fpdf(x,p,n-p),'--','LineWidth',2)
+    plot(x,betapdf(x,p/2,(n-p+1)/2),'--','LineWidth',2)
+    plot(x,fpdf(x,p,n-p+1),'--','LineWidth',2)
     % Add labels and legend
     xlabel('Statistic Value')
     ylabel('Density')
     title(['Simulated vs. Theoretical Distributions for \sigma^2 = ' num2str(sigmas(s))])
-    legend('R-squared','F','Null R-squared','Null F','Location','NorthWest')
+    legend('R-squared','F','Null R-squared','Null F','Location','NorthEast')
     hold off
  end
 
  %%
  % Plot kernel density estimate of simulated distribution
- for s=1:3
-    figure
-    hold on
-    x = linspace(0,1,1000);
-    yR2 = ksdensity(R2_test_two(:,s),x);
-    yF = ksdensity(F_test_two(:,s),x);
-    plot(x,yR2,'LineWidth',2)
-    plot(x,yF,'LineWidth',2)
-    % Add labels and legend
-    xlabel('Statistic Value')
-    ylabel('Density')
-    title(['Simulated Distributions for \sigma^2 = ' num2str(sigmas(s))])
-    legend('R-squared','F','Location','NorthWest')
-    hold off
+ for c=1:2
+     figure
+     %hold on
+     x = linspace(0,1,1000);
+     if c==1
+         for s=1:3
+             hold on
+             yR2 = ksdensity(R2_test_two(:,s),x);
+             plot(x,yR2,'LineWidth',2)
+             hold off
+         end
+         % Add labels and legend
+         xlabel('Statistic Value')
+         ylabel('Density')
+         title('Simulated R-squared distributions')
+         legend(['\sigma^2 = ' num2str(sigmas(1))],['\sigma^2 = ' num2str(sigmas(2))],['\sigma^2 = ' num2str(sigmas(3))], 'Location','NorthWest')
+     else
+         for s=1:3
+             hold on
+             yF = ksdensity(F_test_two(:,s),x);
+             plot(x,yF,'LineWidth',2)
+             hold off
+         end
+         % Add labels and legend
+         xlabel('Statistic Value')
+         ylabel('Density')
+         title('Simulated F distributions')
+         legend(['\sigma^2 = ' num2str(sigmas(1))],['\sigma^2 = ' num2str(sigmas(2))],['\sigma^2 = ' num2str(sigmas(3))], 'Location','NorthWest')
+     end
+    %hold off
 end
