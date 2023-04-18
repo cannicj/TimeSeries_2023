69a316412e176cdcbaacb1079fb9ac73aaf8860a
cannicj <jannicnoel.cavegn@uzh.ch>
2023-04-18 13:38:53 +0200

    finish exercise two, data is there, just run it and then create table / plots

--- Assignment 2/exercise2.m
+++ Assignment 2/exercise2.m
@@ -1,26 +1,27 @@
-% exercise 2 by chatGPT
+% exercise 2, one execution
 %% Define ARMA(3,2) model parameters
-a = [0.2, -0.3, 0.5]; % AR coefficients
+a = [0.2, -0.3, 0.5]'; % AR coefficients
 b = [0.4, -0.6]; % MA coefficients
 sigma = 1; % noise standard deviation
-T = 10; % length of time series, need 100 and 1000
+T = 100; % length of time series, need 100 and 1000
 rep = 1e4;
 
 % Generate ARMA(3,2) data
 Y = armasim(T, sigma, a, b);
 
 %% Part b: Estimate optimal AR(p) using AIC and BIC
-AICmin=1e20; BICmin=1e20;
+AICminAR=1e20; BICminAR=1e20;
 pmax = 10;
-pvecAIC=zeros(rep,1);
-pvecBIC=zeros(rep,1);
-for p=0:pmax
+%pvecAIC=zeros(rep,1);
+%pvecBIC=zeros(rep,1);
+for p=1:pmax
     ar_est = exactarp(Y,p);
     %param=armareg(Y,X,p,0,1);
     lsig2=log(ar_est(end)^2);
     z=p; AIC=lsig2+(2*z)/T; BIC=lsig2 + z*log(T)/T;
-    if AIC<AICmin, AICmin=AIC; pvecAIC(i)=p; end
-    if BIC<BICmin, BICmin=BIC; pvecBIC(i)=p; end
+    disp(["AIC", num2str(AIC)])
+    if AIC<AICminAR, AICminAR=AIC; chosenpAIC = p; end %pvecAIC(p)=p; end
+    if BIC<BICminAR, BICminAR=BIC; chosenpBIC = p;end %pvecBIC(p)=p; end
 end
 
 %%
@@ -34,17 +35,15 @@ fprintf('AIC selected AR(p) = %d\n', opt_aic_p);
 fprintf('BIC selected AR(p) = %d\n', opt_bic_p);
 
 %% Part c: Estimate optimal MA(q) using AIC and BIC
-AICmin=1e20; BICmin=1e20;
+AICminMA=1e20; BICminMA=1e20;
 qmax = 10;
-qvecAIC=zeros(rep,1);
-qvecBIC=zeros(rep,1);
+%qvecAIC=zeros(rep,1);
+%qvecBIC=zeros(rep,1);
 for q = 1:qmax
     %estimate MA(q) with sigma
-    %ma_est = ;
+    ma_est = maq(Y, 2, q);
     lsig2=log(ma_est(end)^2);
     z=q; AIC=lsig2+(2*z)/T; BIC=lsig2 + z*log(T)/T;
-    if AIC<AICmin, AICmin=AIC; qvecAIC(i)=q; end
-    if BIC<BICmin, BICmin=BIC; qvecBIC(i)=q; end
+    if AIC<AICminMA, AICminMA=AIC; chosenqAIC = q; end %qvecAIC(i)=q; end
+    if BIC<BICminMA, BICminMA=BIC; chosenqBIC = q; end %qvecBIC(i)=q; end
 end
-fprintf('AIC selected MA(q) = %d\n', opt_aic_q);
-fprintf('BIC selected MA(q) = %d\n', opt_bic_q);
